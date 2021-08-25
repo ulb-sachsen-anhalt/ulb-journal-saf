892e7acfde38e01f37293262ec02a4b632b6f58f
Axel Bauer <axel.bauer@bibliothek.uni-halle.de>
2021-08-25 17:28:13 +0200

    build required xml files

--- journal2saf.py
+++ journal2saf.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
-import os
-import shutil
 import logging
 import requests
 import warnings
+import pycountry
 
+from pathlib import Path
 from configparser import ConfigParser
-from lxml import etree as ET
 
 warnings.filterwarnings('ignore', message='Unverified HTTPS request')
 
@@ -170,74 +169,105 @@ class ExportSAF:
         self.export_path = e['export_path']
         self.collection = e['collection']
 
-    def _handle_dublin_core_dummy(self, work_dir):
-        dc_dummy_path = os.path.join(work_dir, "dublin_core.xml")
-        dublin_core = ET.Element('dublin_core')
-        el_title = ET.Element('dcvalue')
-        el_title.set('element', 'title')
-        el_title.set('qualifier', 'none')
-        el_title.text = 'DUMMY'
-        el_date = ET.Element('dcvalue')
-        el_date.set('element', 'date')
-        el_date.set('qualifier', 'issued')
-        el_date.text = '1982'
-        dublin_core.append(el_title)
-        dublin_core.append(el_date)
-        self.write_xml_file(dublin_core, dc_dummy_path)
+    @staticmethod
+    def write_xml_file(work_dir, dblcore, name, schema='') -> None:
+        work_dir.mkdir(parents=True, exist_ok=True)
+        pth = work_dir / name
+        dcline = '  <dcvalue element="{}" qualifier="{}"{}>{}</dcvalue>'
+        dcvalues = [dcline.format(*tpl) for tpl in dblcore]
+        if schema != '':
+            schema = f' schema="{schema}"'
 
-    def write_xml_file(self, xml_root, outfile):
-        """ writes xml root pretty printed to outfile """
+        with open(pth, 'w') as fh:
+            fh.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+            fh.write(f'<dublin_core{schema}>\n')
+            fh.write('\n'.join(dcvalues))
+            fh.write('\n</dublin_core>')
 
-        root = ET.ElementTree(xml_root)
+    @staticmethod
+    def write_contens_file(work_dir, file_list) -> None:
+        pth = work_dir / 'contens'
+        with open(pth, 'w') as fh:
+            fh.writelines("{}\n".format(line) for line in file_list)
 
-        xml_string = ET.tostring(root, pretty_print=True, encoding='UTF-8')
-        pretty_parser = ET.XMLParser(
-            resolve_entities=False, strip_cdata=False, remove_blank_text=True)
-        xml_root_new = ET.fromstring(xml_string, pretty_parser)
-        xml_formatted = ET.tostring(
-            xml_root_new, pretty_print=True, encoding='UTF-8').decode('UTF-8')
-
-        dst_dir = os.path.dirname(outfile)
-        if not os.path.exists(dst_dir):
-            os.makedirs(dst_dir)
-
-        with open(outfile, 'w') as dc_file:
-            dc_file.write('<?xml version="1.0" encoding="UTF-8"?>' '\n')
-            dc_file.write(xml_formatted)
-
-    def _handle_contens_file(self, work_dir):
-        outfile = 'contens'
-        dummy_path = os.path.join(work_dir, outfile)
-        with open(dummy_path, 'w') as dc_file:
-            dc_file.write('1234.pdf')
-
-    def _handle_metadata_file(self, work_dir):
-        outfile = 'metadata_local.xml'
-        dummy_path = os.path.join(work_dir, outfile)
-        with open(dummy_path, 'w') as dc_file:
-            dc_file.write('<?xml version="1.0" encoding="UTF-8"?>' '\n')
-            dc_file.write('<dublin_core schema="local">')
-            dc_file.write('</dublin_core>')
-
-    def _handle_collections_file(self, work_dir):
+    @staticmethod
+    def write_collections_file(work_dir, collection) -> None:
         outfile = 'collections'
-        dummy_path = os.path.join(work_dir, outfile)
-        with open(dummy_path, 'w') as dc_file:
-            dc_file.write(self.collection)
+        pth = work_dir / outfile
+        with open(pth, 'w') as fh:
+            fh.write(collection)
 
-    def export(self):
+    @staticmethod
+    def locale2isolang(local_code) -> str:
+        locale = local_code[0:2]
+        lang = pycountry.languages.get(alpha_2=locale)
+        isolang = lang.bibliographic if hasattr(lang, 'bibliographic')\
+            else lang.alpha_3
+        return isolang
+
+    def create_meta_file(self, folder, data) -> None:
+        dcl = []
+        for locale, title in data['title'].items():
+            lang = self.locale2isolang(locale)
+            if title:
+                dcl.append(
+                    ('title', 'none', f' language="{lang}"', title), )
+                dcl.append(
+                    ('title', 'alternative', f' language="{lang}"', title),
+                )
+
+        authors = data.get('authorsString')
+        dcl.append(('contributor', 'author', '', authors), )
+
+        date_ = data.get('datePublished')
+        dcl.append(('date', 'issued', '', date_), )
+        isolang = self.locale2isolang(data.get('locale'))
+        dcl.append(('language', 'iso', '', isolang), )
+        galleys = data.get('galleys')
+        assert len(galleys) == 1
+        galley = galleys[0]
+        file_ = galley.get('file')
+        for locale, desc in file_.get('description').items():
+            lang = self.locale2isolang(locale)
+            if desc:
+                dcl.append(
+                    ('description', 'abstract', f' language="{lang}"', desc), )
+                dcl.append(
+                    ('description', 'abstract', f' language="{lang}"', desc),
+                )
+        dcl.append(('type', 'none', '', file_.get('mimetype')), )
+        self.write_xml_file(folder, dcl, 'dublin_core.xml')
+
+    @staticmethod
+    def download_galley(work_dir, data):
+        pass
+
+    def export(self) -> None:
         for journal in self.journals:
             j_name = journal.url_path
-            logging.info(f"write journal folder '{j_name}'")
-            saf_dir = os.path.join(self.export_path, j_name)
-            os.makedirs(saf_dir, exist_ok=True)
-            row_number = 1
-            item_folder = os.path.join(saf_dir, f'item_{row_number:03}')
-            os.makedirs(item_folder, exist_ok=True)
-            self._handle_dublin_core_dummy(item_folder)
-            self._handle_contens_file(item_folder)
-            self._handle_metadata_file(item_folder)
-            self._handle_collections_file(item_folder)
+            for submission in journal.submissions:
+                status = submission.data['status']
+                if status != 3:  # 3 --> published
+                    continue
+                publ = submission.data['publications']
+                logging.debug(f"write journal folder '{j_name}'")
+                assert len(publ) == 1
+                publ = publ[0]
+
+                item_number = 1
+                item_folder = Path(self.export_path)\
+                    .joinpath(j_name, f'item_{item_number:03}')
+
+                self.create_meta_file(item_folder, publ)
+                schema = 'local'
+                self.write_xml_file(item_folder, [
+                    ('title', 'none', '', 'Hans im mähriſchen Glück'),
+                    ('date', 'issued', '', '1982')], f'metadata_{schema}.xml',
+                    schema="local")
+                self.write_contens_file(item_folder, ['1', '2', 'n'])
+                self.write_collections_file(item_folder, self.collection)
+                self.download_galley(item_folder, publ)
+
 
             # shutil.make_archive(saf_dir, zip, saf_dir)
 
