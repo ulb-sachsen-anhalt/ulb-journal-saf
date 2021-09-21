908d9f0c06edbd8498487e3cae58020ebde2ca0e
Axel Bauer <axel.bauer@bibliothek.uni-halle.de>
2021-09-21 16:39:55 +0200

    format dict

--- tests/ressources/publishers.py
+++ tests/ressources/publishers.py
@@ -1,1 +1,88 @@
-publisher = {'itemsMax': 15, 'items': [{'_href': 'https://www.example.com/cicadina/api/v1/contexts/1', 'abbreviation': {'de_DE': 'Cicadina', 'en_US': 'Cicadina'}, 'acronym': {'de_DE': 'cicadina', 'en_US': 'cicadina'}, 'description': {'de_DE': '<p>Die <strong>Zeitschrift "Cicadina" (vormals "Beiträge zur Zikadenkunde") </strong>ist ein Publikationsorgan für deutsch- Bisher sind 12 Hefte und 1 Supplementheft erschienen (siehe <a href="http://public.bibliothek.uni-halle.de/index.php/cicadina/issue/archive" target="_blank" rel="noopener">Archiv</a>). Ab dem Heft 13 (2013) erscheint die Zeitschrift als <strong>Online-Zeitschrift</strong> (open<span class="SpellE"> access</span>) an der Martin-Luther-Universität Halle-Wittenberg.</p>', 'en_US': '<p>The <strong>Journal „Cicadina“ (formerly „Beiträge zur Zikadenkunde“) </strong>is a forum for covering the (see below and <a href="http://public.bibliothek.uni-halle.de/index.php/cicadina/issue/archive">archive</a>). With issue 13 (2013) the journal is an <strong><a href="http://public.bibliothek.uni-halle.de/index.php/cicadina/index">open access journal</a></strong> hosted by the Martin-Luther University Halle-Wittenberg (Germany).</p>'}, 'enabled': True, 'id': 1, 'journalThumbnail': {'de_DE': {'name': 'cicadina_neu.jpg', 'uploadName': 'journalThumbnail_de_DE.jpg', 'width': 2480, 'height': 3508, 'dateUploaded': '2018-08-10 10:00:12', 'altText': 'cicadina'}, 'en_US': {'name': 'cicadina_neu.jpg', 'uploadName': 'journalThumbnail_en_US.jpg', 'width': 2480, 'height': 3508, 'dateUploaded': '2018-09-03 10:28:30', 'altText': 'cicadina'}}, 'name': {'de_DE': 'Cicadina', 'en_US': 'Cicadina'}, 'seq': 5, 'url': 'https://www.example.com/cicadina', 'urlPath': 'cicadina'}, {'_href': 'https://www.example.com/hdwiso/api/v1/contexts/2', 'abbreviation': {'en_US': '', 'de_DE': ''}, 'acronym': {'de_DE': 'hdwiso', 'en_US': 'hdwiso'}, 'description': {'de_DE': '<p>In den Halleschen Diskussionsbeiträgen zur Wirtschafts- und Sozialgeographie werden wirtschafts- und sozialwissenschaftliche Forschungsergebnisse aus dem Institut für Geographie der Universität Halle-Wittenberg veröffentlicht.</p>', 'en_US': '<p>In den Halleschen Diskussionsbeiträgen zur Wirtschafts- und Sozialgeographie werden wirtschafts- und sozialwissenschaftliche Forschungsergebnisse aus dem Institut für Geographie der Universität Halle-Wittenberg veröffentlicht.</p>'}, 'enabled': True, 'id': 2, 'journalThumbnail': {'de_DE': {'name': 'hdwiso_logo.jpg', 'uploadName': 'journalThumbnail_de_DE.jpg', 'width': 2480, 'height': 3508, 'dateUploaded': '2018-08-03 07:12:10'}, 'en_US': {'name': 'hdwiso_logo.jpg', 'uploadName': 'journalThumbnail_en_US.jpg', 'width': 2480, 'height': 3508, 'dateUploaded': '2018-08-02 11:56:21', 'altText': ''}}, 'name': {'de_DE': 'Hallesche Diskussionsbeiträge zur Wirtschafts- und Sozialgeographie', 'en_US': 'Hallesche Diskussionsbeiträge zur Wirtschafts- und Sozialgeographie'}, 'seq': 5, 'url': 'https://www.example.com/hdwiso', 'urlPath': 'hdwiso'}, ]}
+publisher = {'items': [{'_href': 'https://www.example.com/cicadina/api/v1/contexts/1',
+            'abbreviation': {'de_DE': 'Cicadina', 'en_US': 'Cicadina'},
+            'acronym': {'de_DE': 'cicadina', 'en_US': 'cicadina'},
+            'description': {'de_DE': '<p>Die <strong>Zeitschrift "Cicadina" '
+                                     '(vormals "Beiträge zur Zikadenkunde") '
+                                     '</strong>ist ein Publikationsorgan für '
+                                     'deutsch- Bisher sind 12 Hefte und 1 '
+                                     'Supplementheft erschienen (siehe <a '
+                                     'href="http://public.bibliothek.uni-halle.de/index.php/cicadina/issue/archive" '
+                                     'target="_blank" '
+                                     'rel="noopener">Archiv</a>). Ab dem Heft '
+                                     '13 (2013) erscheint die Zeitschrift als '
+                                     '<strong>Online-Zeitschrift</strong> '
+                                     '(open<span class="SpellE"> '
+                                     'access</span>) an der '
+                                     'Martin-Luther-Universität '
+                                     'Halle-Wittenberg.</p>',
+                            'en_US': '<p>The <strong>Journal „Cicadina“ '
+                                     '(formerly „Beiträge zur Zikadenkunde“) '
+                                     '</strong>is a forum for covering the '
+                                     '(see below and <a '
+                                     'href="http://public.bibliothek.uni-halle.de/index.php/cicadina/issue/archive">archive</a>). '
+                                     'With issue 13 (2013) the journal is an '
+                                     '<strong><a '
+                                     'href="http://public.bibliothek.uni-halle.de/index.php/cicadina/index">open '
+                                     'access journal</a></strong> hosted by '
+                                     'the Martin-Luther University '
+                                     'Halle-Wittenberg (Germany).</p>'},
+            'enabled': True,
+            'id': 1,
+            'journalThumbnail': {'de_DE': {'altText': 'cicadina',
+                                           'dateUploaded': '2018-08-10 '
+                                                           '10:00:12',
+                                           'height': 3508,
+                                           'name': 'cicadina_neu.jpg',
+                                           'uploadName': 'journalThumbnail_de_DE.jpg',
+                                           'width': 2480},
+                                 'en_US': {'altText': 'cicadina',
+                                           'dateUploaded': '2018-09-03 '
+                                                           '10:28:30',
+                                           'height': 3508,
+                                           'name': 'cicadina_neu.jpg',
+                                           'uploadName': 'journalThumbnail_en_US.jpg',
+                                           'width': 2480}},
+            'name': {'de_DE': 'Cicadina', 'en_US': 'Cicadina'},
+            'seq': 5,
+            'url': 'https://www.example.com/cicadina',
+            'urlPath': 'cicadina'},
+           {'_href': 'https://www.example.com/hdwiso/api/v1/contexts/2',
+            'abbreviation': {'de_DE': '', 'en_US': ''},
+            'acronym': {'de_DE': 'hdwiso', 'en_US': 'hdwiso'},
+            'description': {'de_DE': '<p>In den Halleschen '
+                                     'Diskussionsbeiträgen zur Wirtschafts- '
+                                     'und Sozialgeographie werden wirtschafts- '
+                                     'und sozialwissenschaftliche '
+                                     'Forschungsergebnisse aus dem Institut '
+                                     'für Geographie der Universität '
+                                     'Halle-Wittenberg veröffentlicht.</p>',
+                            'en_US': '<p>In den Halleschen '
+                                     'Diskussionsbeiträgen zur Wirtschafts- '
+                                     'und Sozialgeographie werden wirtschafts- '
+                                     'und sozialwissenschaftliche '
+                                     'Forschungsergebnisse aus dem Institut '
+                                     'für Geographie der Universität '
+                                     'Halle-Wittenberg veröffentlicht.</p>'},
+            'enabled': True,
+            'id': 2,
+            'journalThumbnail': {'de_DE': {'dateUploaded': '2018-08-03 '
+                                                           '07:12:10',
+                                           'height': 3508,
+                                           'name': 'hdwiso_logo.jpg',
+                                           'uploadName': 'journalThumbnail_de_DE.jpg',
+                                           'width': 2480},
+                                 'en_US': {'altText': '',
+                                           'dateUploaded': '2018-08-02 '
+                                                           '11:56:21',
+                                           'height': 3508,
+                                           'name': 'hdwiso_logo.jpg',
+                                           'uploadName': 'journalThumbnail_en_US.jpg',
+                                           'width': 2480}},
+            'name': {'de_DE': 'Hallesche Diskussionsbeiträge zur Wirtschafts- '
+                              'und Sozialgeographie',
+                     'en_US': 'Hallesche Diskussionsbeiträge zur Wirtschafts- '
+                              'und Sozialgeographie'},
+            'seq': 5,
+            'url': 'https://www.example.com/hdwiso',
+            'urlPath': 'hdwiso'}],
+ 'itemsMax': 15}
