253c641a444e9c756ca5674c86228623bae5d152
Axel Bauer <axel.bauer@bibliothek.uni-halle.de>
2021-10-04 12:30:11 +0200

    pep8 stuff

--- tests/ressources/issue.py
+++ tests/ressources/issue.py
@@ -1,115 +1,123 @@
 issue = {
     '_href': 'https://ojs.example.com/hsg/api/v1/issues/188',
-    'articles': [{'_href': 'https://ojs.example.com/hsg/api/v1/submissions/103',
-                'contextId': 3,
-                'currentPublicationId': 102,
-                'dateLastActivity': '2018-08-13 08:45:41',
-                'dateSubmitted': '2018-08-13 08:44:32',
-                'id': 103,
-                'lastModified': '2018-08-13 08:45:41',
-                'locale': 'de_DE',
-                'publications': [{'_href': 'https://ojs.example.com/hsg/api/v1/submissions/103/publications/102',
-                                    'authorsString': '',
-                                    'authorsStringShort': 'Schmidt',
-                                    'categoryIds': [],
-                                    'coverImage': {'de_DE': None, 'en_US': None},
-                                    'datePublished': '2018-08-13',
-                                    'fullTitle': {'de_DE': 'Umweltbelastung durch '
-                                                            'Bergbau: Der Einfluß '
-                                                            'der Halden des '
-                                                            'Mansfelder '
-                                                            'Kupferschieferbergbaus '
-                                                            'auf die '
-                                                            'Schwermetallführung '
-                                                            'der Böden und '
-                                                            'Gewässer im '
-                                                            'Einzugsgebiet Süßer '
-                                                            'See',
-                                                'en_US': ''},
-                                    'galleys': [{'file': {'_href': 'https://ojs.example.com/hsg/api/v1/submissions/103/files/398',
-                                                        'assocId': 103,
-                                                        'assocType': 521,
-                                                        'caption': None,
-                                                        'copyrightOwner': None,
-                                                        'createdAt': '2018-08-13 '
-                                                                        '08:45:22',
-                                                        'creator': {'de_DE': '',
-                                                                    'en_US': ''},
-                                                        'credit': None,
-                                                        'dateCreated': None,
-                                                        'dependentFiles': [],
-                                                        'description': {'de_DE': '',
-                                                                        'en_US': ''},
-                                                        'documentType': 'pdf',
-                                                        'fileId': 202,
-                                                        'fileStage': 10,
-                                                        'genreId': 25,
-                                                        'id': 398,
-                                                        'language': None,
-                                                        'locale': 'de_DE',
-                                                        'mimetype': 'application/pdf',
-                                                        'name': {'de_DE': 'admin, '
-                                                                            '975-3170-1-PB.pdf',
-                                                                    'en_US': ''},
-                                                        'path': 'journals/3/articles/103/submission/proof/103-25-398-1-10-20180813.pdf',
-                                                        'publisher': {'de_DE': '',
-                                                                        'en_US': ''},
-                                                        'revisions': [],
-                                                        'source': {'de_DE': '',
-                                                                    'en_US': ''},
-                                                        'sourceSubmissionFileId': None,
-                                                        'sponsor': {'de_DE': '',
-                                                                    'en_US': ''},
-                                                        'subject': {'de_DE': '',
-                                                                    'en_US': ''},
-                                                        'submissionId': 103,
-                                                        'terms': None,
-                                                        'updatedAt': '2018-08-13 '
-                                                                        '08:45:22',
-                                                        'uploaderUserId': 1,
-                                                        'url': 'https://ojs.example.com/hsg/$$$call$$$/api/file/file-api/download-file?submissionFileId=398&submissionId=103&stageId=5',
-                                                        'viewable': False},
-                                                'id': 103,
-                                                'isApproved': True,
-                                                'label': 'PDF',
-                                                'locale': 'de_DE',
-                                                'pub-id::publisher-id': None,
-                                                'publicationId': 102,
-                                                'seq': 0,
-                                                'submissionFileId': 398,
-                                                'urlPublished': 'https://ojs.example.com/hsg/article/view/103/version/102/103',
-                                                'urlRemote': ''}],
-                                    'id': 102,
-                                    'locale': 'de_DE',
-                                    'pages': '',
-                                    'prefix': {'de_DE': '', 'en_US': ''},
-                                    'primaryContactId': 394,
-                                    'pub-id::publisher-id': None,
-                                    'sectionId': 3,
-                                    'status': 3,
-                                    'submissionId': 103,
-                                    'subtitle': {'de_DE': 'Der Einfluß der Halden '
-                                                        'des Mansfelder '
-                                                        'Kupferschieferbergbaus '
-                                                        'auf die '
-                                                        'Schwermetallführung '
-                                                        'der Böden und Gewässer '
-                                                        'im Einzugsgebiet Süßer '
-                                                        'See',
-                                                'en_US': ''},
-                                    'title': {'de_DE': 'Umweltbelastung durch '
-                                                        'Bergbau',
-                                            'en_US': ''},
-                                    'urlPublished': 'https://ojs.example.com/hsg/article/view/103/version/102',
-                                    'version': 1}],
-                'stageId': 5,
-                'status': 3,
-                'statusLabel': 'Veröffentlicht',
-                'submissionProgress': 0,
-                'urlAuthorWorkflow': 'https://ojs.example.com/hsg/authorDashboard/submission/103',
-                'urlEditorialWorkflow': 'https://ojs.example.com/hsg/workflow/access/103',
-                'urlPublished': 'https://ojs.example.com/hsg/article/view/103',
-                'urlWorkflow': 'https://ojs.example.com/hsg/workflow/access/103'}],
+    'articles': [
+        {'_href': 'https://ojs.example.com/hsg/api/v1/submissions/103',
+             'contextId': 3,
+             'currentPublicationId': 102,
+         'dateLastActivity': '2018-08-13 08:45:41',
+         'dateSubmitted': '2018-08-13 08:44:32',
+         'id': 103,
+         'lastModified': '2018-08-13 08:45:41',
+         'locale': 'de_DE',
+             'publications': [{
+                 '_href':
+                 'https://ojs.example.com/hsg/api/v1/submissions/103/publications/102',
+                 'authorsString': '',
+                 'authorsStringShort': 'Schmidt',
+                 'categoryIds': [],
+                 'coverImage': {
+                     'de_DE': None, 'en_US': None},
+                 'datePublished': '2018-08-13',
+                 'fullTitle': {
+                   'de_DE': 'Umweltbelastung durch '
+                     'Bergbau: Der Einfluß '
+                     'der Halden des '
+                     'Mansfelder '
+                     'Kupferschieferbergbaus '
+                     'auf die '
+                     'Schwermetallführung '
+                     'der Böden und '
+                     'Gewässer im '
+                     'Einzugsgebiet Süßer '
+                     'See',
+                     'en_US': ''},
+                 'galleys': [
+                     {'file': {
+                         '_href':
+                         'https://ojs.example.com/hsg/api/v1/submissions/103/files/398',
+                         'assocId': 103,
+                         'assocType': 521,
+                         'caption': None,
+                         'copyrightOwner': None,
+                         'createdAt': '2018-08-13 '
+                         '08:45:22',
+                         'creator': {'de_DE': '',
+                                     'en_US': ''},
+                         'credit': None,
+                         'dateCreated': None,
+                         'dependentFiles': [],
+                         'description': {'de_DE': '',
+                                         'en_US': ''},
+                         'documentType': 'pdf',
+                         'fileId': 202,
+                         'fileStage': 10,
+                         'genreId': 25,
+                         'id': 398,
+                         'language': None,
+                         'locale': 'de_DE',
+                         'mimetype': 'application/pdf',
+                         'name': {'de_DE': 'admin, '
+                                  '975-3170-1-PB.pdf',
+                                  'en_US': ''},
+                         'path': 'journals/3/articles/103/submission/proof/103-25-398-1-10-20180813.pdf',
+                         'publisher': {'de_DE': '',
+                                       'en_US': ''},
+                         'revisions': [],
+                         'source': {'de_DE': '',
+                                    'en_US': ''},
+                         'sourceSubmissionFileId': None,
+                         'sponsor': {'de_DE': '',
+                                     'en_US': ''},
+                         'subject': {'de_DE': '',
+                                     'en_US': ''},
+                         'submissionId': 103,
+                         'terms': None,
+                         'updatedAt': '2018-08-13 '
+                         '08:45:22',
+                         'uploaderUserId': 1,
+                         'url': 'https://ojs.example.com/hsg/$$$call$$$/api/file/file-api/download-file?submissionFileId=398&submissionId=103&stageId=5',
+                         'viewable': False},
+                      'id': 103,
+                      'isApproved': True,
+                      'label': 'PDF',
+                      'locale': 'de_DE',
+                      'pub-id::publisher-id': None,
+                      'publicationId': 102,
+                      'seq': 0,
+                      'submissionFileId': 398,
+                      'urlPublished': 'https://ojs.example.com/hsg/article/view/103/version/102/103',
+                      'urlRemote': ''}],
+                 'id': 102,
+                 'locale': 'de_DE',
+                 'pages': '',
+                 'prefix': {'de_DE': '', 'en_US': ''},
+                 'primaryContactId': 394,
+                 'pub-id::publisher-id': None,
+                 'sectionId': 3,
+                 'status': 3,
+                 'submissionId': 103,
+                 'subtitle': {'de_DE': 'Der Einfluß der Halden '
+                              'des Mansfelder '
+                              'Kupferschieferbergbaus '
+                              'auf die '
+                              'Schwermetallführung '
+                              'der Böden und Gewässer '
+                              'im Einzugsgebiet Süßer '
+                              'See',
+                              'en_US': ''},
+                 'title': {'de_DE': 'Umweltbelastung durch '
+                           'Bergbau',
+                           'en_US': ''},
+                 'urlPublished': 'https://ojs.example.com/hsg/article/view/103/version/102',
+                 'version': 1}],
+             'stageId': 5,
+         'status': 3,
+         'statusLabel': 'Veröffentlicht',
+         'submissionProgress': 0,
+         'urlAuthorWorkflow': 'https://ojs.example.com/hsg/authorDashboard/submission/103',
+         'urlEditorialWorkflow': 'https://ojs.example.com/hsg/workflow/access/103',
+         'urlPublished': 'https://ojs.example.com/hsg/article/view/103',
+         'urlWorkflow': 'https://ojs.example.com/hsg/workflow/access/103'}],
     'coverImageAltText': {'de_DE': '', 'en_US': ''},
     'coverImageUrl': {'de_DE': '', 'en_US': ''},
     'dateNotified': None,
@@ -123,9 +131,9 @@ issue = {
     'number': '0',
     'publishedUrl': 'https://ojs.example.com/hsg/issue/view/188',
     'sections': [{'abbrev': {'en_US': 'ART'},
-                'id': 3,
-                'seq': 0,
-                'title': {'en_US': 'Articles'}}],
+                  'id': 3,
+                  'seq': 0,
+                  'title': {'en_US': 'Articles'}}],
     'title': {'de_DE': 'Umweltbelastung durch Bergbau', 'en_US': ''},
     'volume': 3,
     'year': 1999}
--- tests/ressources/issues.py
+++ tests/ressources/issues.py
@@ -1,34 +1,41 @@
 issues = {'items': [{'_href': 'https://ojs.example.com/hsg/api/v1/issues/188',
-            'coverImageAltText': {'de_DE': '', 'en_US': ''},
-            'coverImageUrl': {'de_DE': '', 'en_US': ''},
-            'datePublished': '2018-08-13 08:45:41',
-            'description': {'de_DE': '', 'en_US': ''},
-            'galleys': [],
-            'id': 188,
-            'identification': 'Bd. 3 (1999): Umweltbelastung durch Bergbau',
-            'number': '0',
-            'publishedUrl': 'https://ojs.example.com/hsg/issue/view/188',
-            'title': {'de_DE': 'Umweltbelastung durch Bergbau', 'en_US': ''},
-            'volume': 3,
-            'year': 1999},
-           {'_href': 'https://ojs.example.com/hsg/api/v1/issues/187',
-            'coverImageAltText': {'de_DE': '', 'en_US': ''},
-            'coverImageUrl': {'de_DE': '', 'en_US': ''},
-            'datePublished': '2018-08-13 08:42:34',
-            'description': {'de_DE': '', 'en_US': ''},
-            'galleys': [],
-            'id': 187,
-            'identification': 'Bd. 2 (1998): Schlammablagerungen in '
-                              'städtischen Fließgewässern : Ursachen, '
-                              'Schwermetallbelastung und Remobilisierbarkeit '
-                              'untersucht',
-            'number': '0',
-            'publishedUrl': 'https://ojs.example.com/hsg/issue/view/187',
-            'title': {'de_DE': 'Schlammablagerungen in städtischen '
+                     'coverImageAltText': {'de_DE': '', 'en_US': ''},
+                     'coverImageUrl': {'de_DE': '', 'en_US': ''},
+                     'datePublished': '2018-08-13 08:45:41',
+                     'description': {'de_DE': '', 'en_US': ''},
+                     'galleys': [],
+                     'id': 188,
+                     'identification':
+                         'Bd. 3 (1999): Umweltbelastung durch Bergbau',
+                     'number': '0',
+                     'publishedUrl':
+                    'https://ojs.example.com/hsg/issue/view/188',
+                     'title': {
+                         'de_DE':
+                         'Umweltbelastung durch Bergbau', 'en_US': ''},
+                     'volume': 3,
+                     'year': 1999},
+                    {'_href':
+                        'https://ojs.example.com/hsg/api/v1/issues/187',
+                     'coverImageAltText': {'de_DE': '', 'en_US': ''},
+                     'coverImageUrl': {'de_DE': '', 'en_US': ''},
+                     'datePublished': '2018-08-13 08:42:34',
+                     'description': {'de_DE': '', 'en_US': ''},
+                     'galleys': [],
+                     'id': 187,
+                     'identification':
+                        'Bd. 2 (1998): Schlammablagerungen in '
+                        'städtischen Fließgewässern : Ursachen, '
+                        'Schwermetallbelastung und Remobilisierbarkeit '
+                        'untersucht',
+                     'number': '0',
+                     'publishedUrl':
+                     'https://ojs.example.com/hsg/issue/view/187',
+                     'title': {'de_DE': 'Schlammablagerungen in städtischen '
                                'Fließgewässern : Ursachen, '
                                'Schwermetallbelastung und Remobilisierbarkeit '
                                'untersucht',
-                      'en_US': ''},
-            'volume': 2,
-            'year': 1998}],
- 'itemsMax': 3}
+                               'en_US': ''},
+                     'volume': 2,
+                     'year': 1998}],
+          'itemsMax': 3}
