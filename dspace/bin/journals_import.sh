249907465cdc9dc844dfb8997ba2b6ca4c0c23b0
Axel Bauer <axel.bauer@bibliothek.uni-halle.de>
2021-11-29 15:38:07 +0100

    initial commit

---
+++ dspace/bin/journals_import.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+#set -e
+
+echo "------------------------------------------------------------------------------"
+echo "Version 0.2.0"
+
+dspace="/opt/dspace/repo/bin/dspace"
+safs="/opt/dspace/repo/infrastructure/ojs_omp/source/"
+maps="/opt/dspace/repo/infrastructure/ojs_omp/map/"
+dois="/opt/dspace/repo/infrastructure/ojs_omp/doi/"
+
+function write_doi() {
+        doifilename=$1
+        doi=$2
+        doifile=$dois$doifilename
+        if test -f "$doifile"; then
+            echo "exists: **$doifile**  proceed..."
+        else
+            echo "$doi" > "$doifile"
+            echo "write doi file -> $doifile"
+        fi
+}
+
+function get_doi() {
+        HANDLE=$1
+        mapfilename=$2
+        echo got Handle: "$HANDLE"
+        DOI=$(/opt/dspace/repo/bin/dspace doi-organiser --list | grep "$HANDLE")
+        echo "die doi Zeile ist ${DOI}"
+        IFS=' ' read -r -a doiline <<< "${DOI}"
+        doi=${doiline[0]}
+        echo "extracted DOI:$doi"
+        write_doi "$mapfilename".doi "$doi"
+}
+
+function get_handle() {
+        if test -f "$1"; then
+           echo "$1"
+           zipfilename=$(basename -- "$1")
+           mapfilename=${zipfilename%%.*}
+
+           IFS=' ' read -r -a array <<< "$(cat "$1")"
+           ISSUE=${array[0]}
+           HANDLE=${array[1]}
+           echo "das issue: $ISSUE"
+           echo "das handle: $HANDLE"
+           get_doi "$HANDLE" "$mapfilename"
+        fi
+}
+
+function import_saf() {
+        saffile=$1
+        echo "import $saffile"
+        cmd="$dspace import --add \
+        --eperson axel.bauer@bibliothek.uni-halle.de \
+        --source $safs \
+        --zip $saffile \
+        --mapfile $maps/$saffile.map\
+        --disable_inheritance"
+        echo $cmd
+        $cmd
+}
+
+for saf in "$safs"/*
+    do
+        safname=$(basename -- "$saf")
+        import_saf "$safname"
+    done
+
+
+for saf in "$safs"/*
+    do
+        safname=$(basename "$saf")
+        mapfile=$maps$safname.map
+        echo "open $mapfile"
+        if test -f "$mapfile"; then
+           get_handle "$mapfile"
+        fi
+    done
