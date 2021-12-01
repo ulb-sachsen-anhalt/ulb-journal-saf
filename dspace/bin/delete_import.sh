860ed651dbb5ada7cd384bec09dd695ef5539ca4
Axel Bauer <axel.bauer@bibliothek.uni-halle.de>
2021-12-01 16:31:39 +0100

    formatting

--- dspace/bin/delete_import.sh
+++ dspace/bin/delete_import.sh
@@ -9,8 +9,8 @@ for mf in "$mapsvolume"/*
    do
      mapfilename=$(basename "$mf")
      mapcontainer=$mapscontainer/$mapfilename
-     echo delete Item with Handel in $mapcontainer
-     docker exec --user dspace $container $dspace import --delete --eperson axel.bauer@bibliothek.uni-halle.de --mapfile $mapcontainer
+     echo delete Item with Handel in "$mapcontainer"
+     docker exec --user dspace $container $dspace import --delete --eperson axel.bauer@bibliothek.uni-halle.de --mapfile "$mapcontainer"
    done
 
 rm -vf ./volumes/infrastructure/ojs_omp/doi/*
