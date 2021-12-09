3a84203f922cb86452d433faebfd83e0e680e694
Axel Bauer <axel.bauer@bibliothek.uni-halle.de>
2021-12-09 16:59:57 +0100

    refactor load of journals

--- journal2saf.py
+++ journal2saf.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+import re
 import logging
 import argparse
 import warnings
 import requests
 import pathlib
 from datetime import datetime
+from pathlib import Path
 from configparser import ConfigParser
 
 from export_saf import ExportSAF
@@ -84,6 +86,18 @@ class DataPoll():
         self.endpoint_issues = g['endpoint_issues']
         self.journal_server = f"{g['journal_server']}/"
         self.token = f"apiToken={g['api_token']}"
+        e = configparser['export']
+        self.export_path = e['export_path']
+
+    def determine_done(self):
+        self.processed = {}
+        export = Path(self.export_path).iterdir()
+        for file_ in export:
+            print(file_.name)
+            parts = re.split('[_.]', file_.name)
+            publication_id = parts[3]
+            submission_file_id = parts[7]
+            self.processed[submission_file_id] = publication_id
 
     def _server_request(self, query) -> dict:
         if '?' in query:
@@ -101,62 +115,75 @@ class DataPoll():
             raise ValueError(result_dct)
         return result_dct
 
-    def rest_call_context(self, journal_name=None, id_=None) -> str:
-        """call *one* or *all* context(s) from server"""
-
-        context = journal_name if journal_name else '_'
-        id_ = f'/{id_}' if id_ else ''
+    def rest_call_contexts(self, offset=0) -> str:
+        """build contexts call for server request"""
+        endpoint = self.endpoint_contexts
+        mark = '&' if '?' in endpoint else '?'
+        endpoint = f"{endpoint}{mark}offset={offset}&isEnabled=true"
         rest_call = ''.join([
-            self.journal_server,
-            context, self.endpoint_contexts, id_])
+            self.journal_server, '_', endpoint])
         logger.info(
             f"build contexts REST call: {rest_call}")
         return rest_call
 
-    def rest_call_issues(self, journal_url, issue_id=None) -> str:
-        """call one or all issue(s) from journal"""
+    def _request_publishers(self) -> None:
+        allitems = 1
+        offset = 0
+        self.items = []
+        while allitems > offset:
+            query_publishers = self.rest_call_contexts(offset)
+            batch_ = self._server_request(query_publishers)
+            logger.info([publ['urlPath'] for publ in batch_['items']])
+            self.items.extend(batch_['items'])
+            allitems = batch_['itemsMax']
+            offset = len(self.items)
+        logger.info(F'got all published items ({len(self.items)}), done...')
 
-        issue_id = f'/{issue_id}' if issue_id else ''
+    def serialise_data(self, start=0, end=-1) -> None:
+        logger.info(f"{len(self.items)} publishers found")
+        for data in self.items[start:end]:
+            publisher = Publisher(data)
+            self.publishers.append(publisher)
+
+    def rest_call_issues(self, journal_url, offset=0) -> str:
+        """build issues call for journal request"""
+        endpoint = self.endpoint_issues
+        mark = '&' if '?' in endpoint else '?'
+        endpoint = f"{endpoint}{mark}offset={offset}&isPublish=true"
         rest_call = ''.join([
-            journal_url, self.endpoint_issues, issue_id])
+            journal_url, endpoint])
         logger.debug(
             "build issues REST call: {rest_call}")
         return rest_call
 
-    def _request_publishers(self) -> None:
-        query_publishers = self.rest_call_context()
-        self.publishers_item_dict = self._server_request(query_publishers)
-
-    def serialise_data(self, start=0, end=-1) -> None:
-        publishers = self.publishers_item_dict
-        if 'items' in publishers.keys():
-            logger.info(f"{len(publishers['items'])} publishers found")
-            for data in publishers['items'][start:end]:
-                publisher = Publisher(data)
-                self.publishers.append(publisher)
-
     def _request_contexts(self) -> None:
         for publisher in self.publishers:
-            id_ = publisher.id
-            publisher_url = publisher.url_path
-            query_context = self.rest_call_context(publisher_url, id_)
-            context_dict = self._server_request(query_context)
+            publisher_url = publisher._href
+            context_dict = self._server_request(publisher_url)
             logger.info(
                 f'got {len(context_dict)} keys/values for {publisher_url}')
             publisher._data.update(context_dict)
 
     def _request_issues(self) -> None:
         for publisher in self.publishers:
-            query_issues = self.rest_call_issues(publisher.url)
-            logger.info(f'request all issues for {publisher.url_path}:'
-                        f' {query_issues}')
-            issues_dict = self._server_request(query_issues)
-            logger.info(f'receive {issues_dict.get("itemsMax", 0)} issues')
+            allissues = 1
+            offset = 0
+            issues_dict = {'items': []}
+            while allissues > offset:
+                query_issues = self.rest_call_issues(publisher.url, offset)
+                logger.info(f'request all issues for {publisher.url_path}:'
+                            f' {query_issues}')
+                batch_ = self._server_request(query_issues)
+                issues_dict['items'].extend(batch_['items'])
+                allissues = batch_['itemsMax']
+                offset = len(issues_dict['items'])
+            logger.info('got {} issues'.format(len(issues_dict['items'])))
 
             for issue in issues_dict['items']:
                 issue_data = self._server_request(issue['_href'])
-
-                for article in issue_data['articles']:
+                href = issue.get('_href')
+                logger.debug(f'process issue {href}')
+                for article in issue_data['articles']:  # just one article
                     status = article['status']
                     if status != 3:  # 3 means "published"
                         logger.info(
@@ -180,6 +207,11 @@ class DataPoll():
                                 # no need for further processing
                                 del publication['galleys'][index]
                                 continue
+                            file_id = str(galley['submissionFileId'])
+                            publ_id = str(galley['publicationId'])
+                            if publ_id == self.processed.get(file_id):
+                                logger.warning(f'file exist in export {publ_href}')
+                                continue
                             issue_data['galley'] = galley
 
                 issue.update(issue_data)
@@ -191,8 +223,9 @@ class DataPoll():
 
 def data_poll() -> DataPoll:
     dp = DataPoll(CP)
+    dp.determine_done()
     dp._request_publishers()
-    dp.serialise_data(5, 6)
+    dp.serialise_data(1, 2)
     dp._request_issues()
     dp._request_contexts()
     return dp
@@ -223,9 +256,9 @@ def main() -> None:
     start = datetime.now()
     datapoll = data_poll()
     export_saf(datapoll)
-    copy_saf(CP)
-    retrieve_doi(CP)
-    write_remote_url(CP)
+    #copy_saf(CP)
+    #retrieve_doi(CP)
+    #write_remote_url(CP)
 
     end = datetime.now()
     logger.info(f"Elapsed time: {str(end-start).split('.')[0]}")
