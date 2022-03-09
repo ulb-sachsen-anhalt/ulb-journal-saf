120e8982a68bb8fb98a2725a25a673208a24fce8
Axel Bauer <axel.bauer@bibliothek.uni-halle.de>
2022-03-09 17:09:49 +0100

    remove useless brackets

--- dspace/bin/IMPORT_OMP.sh
+++ dspace/bin/IMPORT_OMP.sh
@@ -7,11 +7,11 @@ echo "put lock file $lockfile"
 touch $lockfile
 sleep 3
 
-while  [[ "$(ls -l $export_folder | wc -l)" -gt 2 ]]
+while [ "$(ls -l $export_folder | wc -l)" -gt 2 ]
 do
         echo $(date +%H:%M:%S)
-        echo "wait further 3 seconds"
-        sleep  3
+        echo "wait further 5 seconds"
+        sleep  5
 done;
 
 echo "ok, folder is empty --> start import"
