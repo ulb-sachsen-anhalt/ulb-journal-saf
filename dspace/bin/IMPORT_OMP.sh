bd4470abb908486072925013864274f27f3ef5db
Axel Bauer <axel.bauer@bibliothek.uni-halle.de>
2022-03-09 17:03:43 +0100

    inital commit

---
+++ dspace/bin/IMPORT_OMP.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export_folder=/opt/dspace/volumes/shared_kitodo/migration/working/
+lockfile="$export_folder/STOP_FOR_OMP_IMPORT"
+
+echo "put lock file $lockfile"
+touch $lockfile
+sleep 3
+
+while  [[ "$(ls -l $export_folder | wc -l)" -gt 2 ]]
+do
+        echo $(date +%H:%M:%S)
+        echo "wait further 3 seconds"
+        sleep  3
+done;
+
+echo "ok, folder is empty --> start import"
+
+docker exec -it -u dspace dspace_dspace_1 /bin/bash /opt/dspace/repo/bin/ojs_omp/journals_import.sh omp
+
+sleep 3
+echo "delete $lockfile"
+rm $lockfile
+echo "done..."
